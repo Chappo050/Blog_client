6fa123c5192a0e7e9d4cd6171ba95a0185e2a86d
Chappo050 <matthewchaplin60@gmail.com>
2022-08-31 19:44:03 +1000

    Posts now showing

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -1,26 +1,63 @@
-import React, { useEffect } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Nav from "./Nav";
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/blog'
-})
+  baseURL: "http://localhost:5000/blog",
+});
+
+interface User {
+  _id: String;
+  username: String;
+}
+
+interface Post {
+  _id: String;
+  user_details: User;
+  message: String;
+  post_time: String;
+  public: Boolean;
+}
+
+const defaultPost: Post[] = [];
 
 function Blog() {
+  const [posts, setPosts]: [Post[], (posts: Post[]) => void] =
+    useState(defaultPost);
 
-  useEffect(()=> {
-    api.get('/').then(res => {
-      console.log(res.data);
-    })
-  }, [])
-
+  useEffect(() => {
+    api.get("/").then((res) => {
+      setPosts(res.data);
+    });
+  }, []);
 
   return (
-    <div >
+    <div>
+      <div>
         <Nav />
-Blog
+      </div>
+      <div className=" grid grid-cols-5">
+        <i />
+        <div className="text-2xl text-center col-span-3">
+          {posts ? posts.map((post) => PostContainer(post)) : <h1>No posts yet</h1>}
+        </div>
+
+        <i />
+      </div>
     </div>
   );
 }
+
+const PostContainer = (post: Post) => {
+  return (
+    <div className="border border-custom-silver mt-3 p-3">
+      <div>
+        <h2>Posted By: {post.user_details.username}</h2>
+        <h2>Post time: {post.post_time}</h2>
+        <h2>Message: {post.message}</h2>
+      </div>
+    </div>
+  );
+};
 
 export default Blog;
