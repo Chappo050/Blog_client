10b084ca084f2c74f2e3ad42fba53fcbb5c86a87
Chappo050 <matthewchaplin60@gmail.com>
2022-09-02 19:43:49 +1000

    Pages

--- src/components/Blog/BlogOverview.tsx
+++ src/components/Blog/BlogOverview.tsx
@@ -4,7 +4,6 @@ import axios from "axios";
 import Nav from "../Nav";
 const { DateTime } = require("luxon");
 
-
 const api = axios.create({
   baseURL: "http://localhost:5000/blog/overview",
 });
@@ -28,11 +27,56 @@ function BlogOverview() {
   const [posts, setPosts]: [Post[], (posts: Post[]) => void] =
     useState(defaultPost);
 
+  const [count, setCount] = useState(0);
+
+  const [postPointer, setPostsPointer] = useState(0);
+
+  //Initialize data
   useEffect(() => {
-    api.get("/").then((res) => {
-      setPosts(res.data);
+    api
+      .get("/", {
+        params: {
+          pointer: postPointer,
+        },
+      })
+      .then((res) => {
+        setPosts(res.data);
+      });
+
+    api.get("/count").then((res) => {
+      setCount(res.data);
     });
   }, []);
+
+  //get data for next page
+  useEffect(() => {
+    api
+      .get("/", {
+        params: {
+          pointer: postPointer,
+        },
+      })
+      .then((res) => {
+        setPosts(res.data);
+      });
+    window.scrollTo(0, 0);
+  }, [postPointer]);
+
+  function getNextSet() {
+    //increment pointer to get next 10
+    if (postPointer + 10 > count) {
+      return;
+    }
+    setPostsPointer(postPointer + 10);
+  }
+
+  function getPrevSet() {
+    if (postPointer < 10) {
+      return;
+    }
+    //increment pointer to get prev 10
+    setPostsPointer(postPointer - 10);
+  }
 
   return (
     <div>
@@ -45,11 +89,25 @@ function BlogOverview() {
           ) : (
             <h1>No posts yet</h1>
           )}
+          <div className="pt-5">
+            <button
+              className="text-4xl hover:bg-custom-dark-blue w-10 "
+              onClick={getPrevSet}
+            >
+              {"<"}
+            </button>
+            <i className="w-5 px-10" />
+            <button
+              className="text-4xl hover:bg-custom-dark-blue w-10"
+              onClick={getNextSet}
+            >
+              {">"}
+            </button>
+          </div>
         </div>
 
         <i />
       </div>
-     
     </div>
   );
 }
@@ -60,13 +118,15 @@ const PostContainer = (post: Post) => {
   return (
     <div className="border border-custom-silver mt-3 p-3">
       <div>
-        <div className="grid grid-cols-2 text-base " >
+        <div className="grid grid-cols-2 text-base ">
           <div className=" text-left">
-           Posted By: {post.user_details.username}
+            Posted By: {post.user_details.username}
           </div>
-          <div className="text-right">Post time: {DateTime.fromISO(post.post_time).toFormat('ff')}</div>
+          <div className="text-right">
+            Post time: {DateTime.fromISO(post.post_time).toFormat("ff")}
+          </div>
         </div>
-        <br/>
+        <br />
         <div className="text-xl break-words">{post.message}</div>
       </div>
     </div>
